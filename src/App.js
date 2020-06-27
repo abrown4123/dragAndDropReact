608fa962f295a5b7143808d703f16e068bfe83b6
Anthony <abrown4123@gmail.com>
2020-06-27 16:42:31 -0500

    Connect Components in App and add styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import Board from './components/Board';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
       <main className="flexbox">
-        Here will be boards
+        <Board id="board1" className="board">
+          <Card id="card-1" className="card" draggable="true">
+            <p>Card one</p>
+          </Card>
+        </Board>
+
+        <Board id="board2" className="board">
+          <Card id="card-2" className="card" draggable="true">
+            <p>Card two</p>
+          </Card>
+        </Board>
       </main>
     </div>
   );
